82bfd9dbea5fcdb6f92746da27b1bd84f255f60f
emilpaulitz <57809013+emilpaulitz@users.noreply.github.com>
2026-06-12 19:21:43 +0200

    Setup

---
+++ startup.m
@@ -0,0 +1,28 @@
+%% User-specific paths
+% If you have access to gurobi, add the correct path to gurobi, otherwise
+% leave it empty. For example:
+% gurobiPath = '/opt/gurobi1103/linux64/matlab';
+gurobiPath = '';
+% Change the path to cobratoolbox installation
+pathCobraToolbox = '';
+
+%% global variables
+% projDir
+projDir = pwd;
+gurobiAvailable = isempty(gurobiPath);
+
+%% setup
+% cobratoolbox
+if isempty(pathCobraToolbox)
+    error(['cobratoolbox is required for this project. Please set the' ...
+          'appropriate path in startup.m!']);
+end
+cd pathCobraToolbox;
+initCobraToolbox
+cd(projDir)
+
+% gurobi
+if gurobiAvailable
+    addpath gurobiPath
+    gurobi_setup
+end
